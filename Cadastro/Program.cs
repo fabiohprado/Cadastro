42037b0c62a92399cec7a1f97953b522b716b07e
fabiohprado <fabiohmprado@gmail.com>
2018-11-21 11:48:15 -0200

    inicio do programa

---
+++ Cadastro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tela C;
+
+            Console.WriteLine("Cadastro de  entrada");
+            Console.WriteLine("insira o dados nescessarios");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("CPF: ");
+            int cpf = int.Parse(Console.ReadLine());
+            Console.Write("Endereco: ");
+            string endereco = Console.ReadLine();
+
+            C = new Tela(nome, cpf, endereco);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Informacoes adicionadas sao: ");
+
+            
+        }
+    }
+}
---
+++ Cadastro/Tela.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cadastro
+{
+    class Tela
+    {
+        public string nome { get; set; }
+        public int cpf { get; set; }
+        public string endereco { get; set; }
+
+        public Tela(string nome, int cpf, string endereco)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.endereco = endereco;
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + nome
+                + "CPF: "
+                + cpf
+                + "Endereco: "
+                + endereco;
+        }
+
+    }
+}
